ca05745a301a97d6b639ebe98d42ba123e7930ea
matias <matiasleites@live.com.ar>
2025-03-31 21:50:09 -0300

    fix: hero style positions

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import VideoPlayer from "./videoPlayer/VideoPlayer";
 
 export default function HeroSection() {
   const { t } = useTranslation();
@@ -8,12 +9,15 @@ export default function HeroSection() {
       <div className="d-flex w-100">
         <Row className="w-100 my-auto">
           <Col sm="1" lg="1" />
-          <Col sm="6" className="text-center">
-            <h1>{t("appName")}</h1>
+          <Col sm="6" className="text-start">
+            <h1 className="text-uppercase">{t("appName")}</h1>
+            <VideoPlayer />
           </Col>
-          <Col sm="4" className="text-center">
-            <h4>{t("appSlogan")}</h4>
-            <p dangerouslySetInnerHTML={{ __html: t("heroText") }} />
+          <Col sm="4" className="text-start d-flex">
+            <div className="my-auto">
+              <h2>{t("appSlogan")}</h2>
+              <p dangerouslySetInnerHTML={{ __html: t("heroText") }} />
+            </div>
           </Col>
           <Col sm="1" />
         </Row>
---
+++ src/components/videoPlayer/VideoPlayer.tsx
@@ -0,0 +1,13 @@
+export default function VideoPlayer() {
+  return (
+    <div
+      className="bg-black w-100 d-flex rounded"
+      style={{ minHeight: "55vh" }}
+    >
+      <div className="m-auto">
+        <h1>Video Player</h1>
+        <p>This is a video player component.</p>
+      </div>
+    </div>
+  );
+}
