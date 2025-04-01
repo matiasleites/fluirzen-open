c457f39cee2a560503b3d300510abe2a56f12e45
matias <matiasleites@live.com.ar>
2025-03-31 21:22:23 -0300

    feat adding new structure

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,23 @@
+import { Col, Row } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+
+export default function HeroSection() {
+  const { t } = useTranslation();
+  return (
+    <section id="hero" className="d-flex" style={{ minHeight: "100vh" }}>
+      <div className="d-flex w-100">
+        <Row className="w-100 my-auto">
+          <Col sm="1" lg="1" />
+          <Col sm="6" className="text-center">
+            <h1>{t("appName")}</h1>
+          </Col>
+          <Col sm="4" className="text-center">
+            <h4>{t("appSlogan")}</h4>
+            <p dangerouslySetInnerHTML={{ __html: t("heroText") }} />
+          </Col>
+          <Col sm="1" />
+        </Row>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,13 @@
-import { useTranslation } from "react-i18next";
 import back from "../assets/images/back-sky.webp";
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
-  const { t } = useTranslation();
   return (
     <div
       className="text-center text-white"
       style={{ backgroundImage: `url(${back})`, minHeight: "250vh" }}
     >
-      <section className="d-flex" style={{ minHeight: "100vh" }}>
-        <div className="m-auto">
-          <h1>{t("appName")}</h1>
-          <h4>{t("appSlogan")}</h4>
-          <p>{t("underConstructionInfo")}</p>
-        </div>
-      </section>
+      <HeroSection />
     </div>
   );
 }
