57003fe0382e0e77a9eb71e32b256b8603c93210
Matias Leites <matiasleites@live.com.ar>
2025-04-02 11:22:19 -0300

    feat: adding VideoPlayer image and buttons logic

--- src/components/videoPlayer/VideoPlayerPreview.tsx
+++ src/components/videoPlayer/VideoPlayerPreview.tsx
@@ -1,14 +1,66 @@
 import { t } from "i18next";
+import { useState } from "react";
+import { AudioType, audioTypes } from "../../types/AudioType";
+import { FaRegCirclePlay } from "react-icons/fa6";
+import forest from "../../assets/images/forest-player.webp";
+import music from "../../assets/images/music-player.webp";
+import ocean from "../../assets/images/ocean-player.webp";
+import river from "../../assets/images/river-player.webp";
 
 export default function VideoPlayerPreview() {
+  const [type, setType] = useState<AudioType>("forest");
+
+  const audioConfig = {
+    forest: {
+      image: forest,
+    },
+    river: {
+      image: river,
+    },
+    ocean: {
+      image: ocean,
+    },
+    music: {
+      image: music,
+    },
+  } as const;
+
+  function chooseType(currentType: AudioType) {
+    setType(currentType);
+  }
+
+  const selectedBack = audioConfig[type].image;
+
   return (
     <div
-      className="bg-black w-100 d-flex rounded"
-      style={{ minHeight: "55vh" }}
+      className="bg-black w-100 d-flex flex-column rounded"
+      style={{
+        transition: "all 0.3s",
+        minHeight: "55vh",
+        backgroundImage: `url(${selectedBack})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
     >
-      <div className="m-auto">
-        <h1>Video Player</h1>
-        <p className="text-center">{t("underConstructionInfo")}</p>
+      <div
+        className="mt-1 ms-1 d-flex pointer-purple"
+        style={{ position: "absolute" }}
+      >
+        {audioTypes.map((typeItem) => (
+          <div className="me-2 px-2" onClick={() => chooseType(typeItem)}>
+            <p
+              className="text-uppercase pointer-purple"
+              style={{ color: type === typeItem ? "white" : "gray" }}
+            >
+              {t(`audioTypes.${typeItem}`)}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div className="m-auto d-flex flex-column pointer-purple">
+        <FaRegCirclePlay className="text-center mx-auto " size={56} />
+        <p className="text-center text-uppercase">{t("start")}</p>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import Home from "./pages/Home.tsx";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./locales/i18n";
 import { AppProvider } from "./context/AppProvider.tsx";
+import { IconContext } from "react-icons";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <AppProvider>
-        <Home />
-      </AppProvider>
-    </I18nextProvider>
+    <IconContext.Provider value={{ className: "fluirzen-icons" }}>
+      <I18nextProvider i18n={i18n}>
+        <AppProvider>
+          <Home />
+        </AppProvider>
+      </I18nextProvider>
+    </IconContext.Provider>
   </StrictMode>
 );
