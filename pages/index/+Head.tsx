fa2a83660212ccf7c43cb2ae8c41d571b3d016dd
Matias Leites <matiasleites@live.com.ar>
2025-05-27 13:51:02 -0300

    feat: adding google g-tag

--- pages/index/+Head.tsx
+++ pages/index/+Head.tsx
@@ -6,6 +6,20 @@ export default function HeadDefault() {
   return (
     <>
       <meta property="og:image" content={ogImageUrl} />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-H5T6KDHQJK"></script>
+      <link rel="canonical" href="https://fluirzen.com" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", "G-H5T6KDHQJK");
+      `,
+        }}
+      />
     </>
   );
 }
